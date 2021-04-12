787a7643d609a161c9ac3e46606fb7922df76163
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-12 21:12:43 +0530

    Redirecting requests

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 //import http
 const http = require("http");
+const fs = require('fs');
 
 //create Server
 const server = http.createServer((req,res)=>{
     const url = req.url;
+    const method = req.method;
+
     res.setHeader('Content-Type','text/html');
     if(url == '/'){
         res.write("<html><head><title>Enter Message</title></head><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body></html>");
+        return res.end();
+    }
+
+    if(url=='/message' && method == 'POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode=302;
+        res.setHeader('Location','/');
         return res.end();
     }
     
