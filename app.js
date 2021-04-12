9e4fa87207d4c4fa895513239fe954d83b156616
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-12 20:38:38 +0530

    removed process.exit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const http = require("http");
 //create Server
 const server = http.createServer((req,res)=>{
     console.log("Server Started.");
-    process.exit();
 });
 
 //start listening on port
