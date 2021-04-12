04ad1203cf052ae6dc356b7c60ac903b4373ad04
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-12 20:52:08 +0530

    Sending responses

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const http = require("http");
 //create Server
 const server = http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write("Hello World");
 });
 
 //start listening on port
