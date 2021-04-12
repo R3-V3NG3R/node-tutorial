001fd0d48415be47a43c7d37c9e86bf255d91ed0
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-12 21:49:40 +0530

    Modularization in Node

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 //import http
 const http = require("http");
-const fs = require('fs');
+
+const routes = require('./routes');
+
 
 //create Server
-const server = http.createServer((req,res)=>{
-    const url = req.url;
-    const method = req.method;
-
-    res.setHeader('Content-Type','text/html');
-    if(url == '/'){
-        res.write("<html><head><title>Enter Message</title></head><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body></html>");
-        return res.end();
-    }
-
-    if(url=='/message' && method == 'POST'){
-        const body =[];
-        req.on('data',chunk=>{
-            body.push(chunk);
-        });
-
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.slice('=')[1];
-            fs.writeFileSync('message.txt',message);
-        });
-        
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-    
-    res.write("<html><head><title>Welcom to My Node Server</title></head><body><h1>Welcom to node.js</h1></body></html>");
-    res.end();
-});
+const server = http.createServer(routes);
 
 //start listening on port
 server.listen(3000);
