39eb1def934689512e0505082f6ad556c973fccb
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-12 20:42:23 +0530

    understanding requests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 //create Server
 const server = http.createServer((req,res)=>{
-    console.log("Server Started.");
+    console.log(req.url, req.method, req.headers);
 });
 
 //start listening on port
