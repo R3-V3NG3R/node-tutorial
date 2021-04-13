b227505a1566445fb98a7ac424019d073a77ea72
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-13 15:28:04 +0530

    path filtering

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/add-products',(req, res, next)=>{
     console.log('First middle ware');
-    res.send('<h1>Products page</h1><form action="/products" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>');
+    res.send('<h1>Products page</h1><form action="products" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>');
 
 });
 
