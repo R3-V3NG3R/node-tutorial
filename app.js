93eb7af4dd9d5fcc66c05020e116ba062f885b9f
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-13 15:12:59 +0530

    accepting particular requests

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/add-products',(req, res, next)=>{
 
 });
 
-app.use('/products',(req, res, next)=>{
+app.post('/products',(req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
 
