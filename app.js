c616e1d0b44348259ba3d29cb548427ff902511c
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-13 14:45:19 +0530

    server using express and adding middle ware

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+// //import http
+// const http = require("http");
+
+// const routes = require('./routes');
+
+
+// //create Server
+// const server = http.createServer(routes);
+
+// //start listening on port
+// server.listen(3000);
+
 //import http
 const http = require("http");
 
-const routes = require('./routes');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('First middle ware');
+    next();
+});
+app.use((req, res, next)=>{
+    console.log('second middle ware');
+});
+
 
 
 //create Server
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 //start listening on port
 server.listen(3000);
