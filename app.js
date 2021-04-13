f6f27e8948d8e413c29589ebede42ae8fd371cc8
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-13 20:02:06 +0530

    added HBS configuration

--- app.js
+++ app.js
@@ -15,13 +15,16 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.set('view engine','pug');
+app.engine('handlebars',expressHbs());
+app.set('view engine','handlebars');
+app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
