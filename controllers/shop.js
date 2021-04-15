2f5f668423912d32022af80759e090722f4706f7
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-15 14:00:01 +0530

    Updated Views and css

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,10 +19,15 @@ exports.getCart = (req, res, next)=>{
     res.render('shop/cart',{docTitle:'Cart',path:'/cart'});
 };
 
+
+exports.getOrders = (req, res, next)=>{
+    res.render('shop/orders',{docTitle:'Orders',path:'/orders'});
+};
+
 exports.getCheckout = (req, res, next)=>{
     res.render('shop/checkout',{docTitle:'Checkout',path:'/checkout'});
 };
 
-exports.getCheckout = (req, res, next)=>{
+exports.getProductDetails = (req, res, next)=>{
     res.render('shop/product-detail',{docTitle:'Product Details',path:'/product-detail'});
 };
--- routes/shop.js
+++ routes/shop.js
@@ -7,6 +7,7 @@ router.get('/',shopController.getIndex);
 router.get('/products',shopController.getProducts);
 router.get('/cart',shopController.getCart);
 router.get('/checkout',shopController.getCheckout);
-router.get('/product-detail',shopController.getCheckout);
+router.get('/product-detail',shopController.getProductDetails);
+router.get('/orders',shopController.getOrders);
 
 module.exports = router;
