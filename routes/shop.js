b26c66fe13f7b6d6a7e6a4e74f576f7694f40db2
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-15 14:08:39 +0530

    added product id to path

--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,7 @@ const shopController = require('../controllers/shop');
 
 router.get('/',shopController.getIndex);
 router.get('/products',shopController.getProducts);
+router.get('/products/:productId',shopController.getProducts);
 router.get('/cart',shopController.getCart);
 router.get('/checkout',shopController.getCheckout);
 router.get('/product-detail',shopController.getProductDetails);
