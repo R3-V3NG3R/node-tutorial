7dfb2c3c4d1f7bc1aa1f1a921c71644e64eaf4b4
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-15 11:10:51 +0530

    Adding new views and updating navigation

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProducts = (req, res, next)=>{
-    res.render('add-product',{docTitle:'Add Product',path:'/admin/add-product'});
+    res.render('admin/add-product',{docTitle:'Add Product',path:'/admin/add-product'});
 };
 
 exports.postAddProduct = (req, res, next)=>{
@@ -12,7 +12,7 @@ exports.postAddProduct = (req, res, next)=>{
 
 exports.getProducts = (req, res, next)=>{
     Product.fetchAll(products=>{
-        res.render('shop',{docTitle:"Shop",prods:products,path:'/',hasProducts:products.length>0});
+        res.render('shop/product-list',{docTitle:"Shop",prods:products,path:'/',hasProducts:products.length>0});
     });
     
 };
