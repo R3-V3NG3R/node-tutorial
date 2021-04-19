f4fe06a5559957597fe8466aa3b23598cda73b5c
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-04-19 15:46:57 +0530

    Configured mysql database

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,49 +1,84 @@
-const { reset } = require('nodemon');
 const Product = require('../models/product');
 const Cart = require('../models/cart');
 
-exports.getIndex = (req, res, next)=>{
-    Product.fetchAll(products=>{
-        res.render('shop/index',{docTitle:"Shop",prods:products,path:'/',hasProducts:products.length>0});
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
     });
-    
+  });
 };
 
-exports.getProducts = (req, res, next)=>{
-    Product.fetchAll(products=>{
-        res.render('shop/product-list',{docTitle:"Products List",prods:products,path:'/products',hasProducts:products.length>0});
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
     });
+  });
 };
 
-exports.getProduct = (req, res, next)=>{
-    const prodId = req.params.productId;
-    Product.findById(prodId,product=>{
-        res.render('shop/product-detail', {product:product,docTitle:product.title,path:'/products'});
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
     });
+  });
 };
 
-
-exports.getCart = (req, res, next)=>{
-    res.render('shop/cart',{docTitle:'Cart',path:'/cart'});
+exports.getCart = (req, res, next) => {
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
+  });
 };
 
-exports.postCart = (req, res, next)=>{
-    const prodId = req.body.productId;
-    Product.findById(prodId,(product)=>{
-        Cart.addProduct(prodId, product.price);
-    });
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
+  });
 };
 
-
-exports.getOrders = (req, res, next)=>{
-    res.render('shop/orders',{docTitle:'Orders',path:'/orders'});
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
 };
 
-exports.getCheckout = (req, res, next)=>{
-    res.render('shop/checkout',{docTitle:'Checkout',path:'/checkout'});
-};
-
-exports.getProductDetails = (req, res, next)=>{
-    res.render('shop/product-detail',{docTitle:'Product Details',path:'/product-detail'});
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,24 @@
-const express = require('express');
 const path = require('path');
 
+const express = require('express');
 
-const router = express.Router();
 const adminController = require('../controllers/admin');
 
+const router = express.Router();
 
-router.get('/add-product',adminController.getAddProducts);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product',adminController.postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-router.get('/products',adminController.getAdminProducts);
-router.get('/edit-product/:productId',adminController.getEditProduct);
-router.post('/edit-product',adminController.postEditProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
-module.exports= router;
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
